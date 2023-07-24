7bfd26dd1e0640b313a16055622c727f0091534b
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2023-07-24 17:56:03 +0600

    Update split_training_text.py

--- split_training_text.py
+++ split_training_text.py
@@ -5,6 +5,9 @@ import subprocess
 
 training_text_file = 'langdata/eng.training_text'
 fontconf_dir = '/'
+xsize = 4000
+ysize = 700
+font = ""
 lines = []
 
 with open(training_text_file, 'r') as input_file:
@@ -18,7 +21,7 @@ if not os.path.exists(output_directory):
 
 random.shuffle(lines)
 
-count = 100
+count = 32020
 
 lines = lines[:count]
 
@@ -31,7 +34,7 @@ for line in lines:
 
     file_base_name = f'eng_{line_count}'
 
-    ln = os.system(f'text2image --text={line_training_text} --outputbase={output_directory}/{file_base_name} --max_pages=1 --strip_unrenderable_words --leading=32 --xsize=4000 --ysize=700 --char_spacing=1.0 --exposure=0 --unicharset_file=langdata/eng.unicharset --fontconfig_tmpdir={fontconf_dir} --font="Red Hat Mono" 2>/dev/null')
+    ln = os.system(f'text2image --text={line_training_text} --outputbase={output_directory}/{file_base_name} --max_pages=1 --strip_unrenderable_words --leading=32 --xsize={xsize} --ysize={ysize} --char_spacing=1.0 --exposure=0 --unicharset_file=langdata/eng.unicharset --fontconfig_tmpdir={fontconf_dir} --font="{font}" 2>/dev/null')
     if ln == 0:
         print(f'successfully created: eng_{line_count}.tif')
         print(f'successfully created: eng_{line_count}.gt.txt')
